1bf274e4c19da4684012d337271e7aad1f68dcab
Husam Machlovi <hmachlovi@gmail.com>
2015-05-04 01:17:45 -0400

    package and class imports

---
+++ the_definitive_guide/download_manager.java
@@ -0,0 +1,12 @@
+importPackage(org.mozilla.javascript.tools.Main);
+/*
+* A download manager application with a simple Java GUI
+*/
+
+// Import the Swing GUI components and a few other classes
+importPackage(javax.swing);
+importClass(javax.swing.border.EmptyBorder);
+importClass(java.awt.event.ActionListener);
+importClass(java.net.URL);
+importClass(java.io.FileOutputStream);
+importClass(java.lang.Thread);
