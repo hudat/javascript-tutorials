615b6d85fef55730ab6843f186950ce10034fff5
Husam Machlovi <hmachlovi@gmail.com>
2015-05-25 20:56:40 -0400

    for loop that prints multiples of 3 from 1 to 200

--- exercises/script.js
+++ exercises/script.js
@@ -1,1 +1,10 @@
-alert('Hello world!');
+//Print out in reverse order, every multiple of 3 between 1 and 200.
+
+console.log('Hello world!');
+
+for (var number = 1; number <= 200; number = number + 1) {
+
+  if (number % 3 == 0) {
+    console.log(number);
+  }
+}
