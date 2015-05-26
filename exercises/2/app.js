a883e1f1ebcfca021fe0f1c53e66accb5a54e807
Husam Machlovi <hmachlovi@gmail.com>
2015-05-26 10:47:52 -0400

    updated folder struct, added basic mark-up

--- exercises/2/app.js
+++ exercises/2/app.js
@@ -1,21 +1,3 @@
-console.log('Hello world!');
-
-//Print out in reverse order, every multiple of 3 between 1 and 200.
-
-var multiplesOfThree = [];
-
-for (var number = 1; number <= 200; number = number + 1) {
-  if (number % 3 == 0) {
-    multiplesOfThree.push(number);
-  }
-}
-
-multiplesOfThree = multiplesOfThree.reverse();
-
-for (var n = 0; n < multiplesOfThree.length; n = n + 1) {
-  console.log(multiplesOfThree[n])
-}
-
 
 //Print the numbers from 1 to 100.
 //For multiples of three print "Fizz" instead of the number.
